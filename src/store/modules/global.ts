206a88d68bda887ae7acdef0c607e09fa6564ab7
liuguanhua <lghayy@foxmail.comx>
2022-10-30 16:38:52 +0800

    修改登录侧边栏展开模式

--- src/store/modules/global.ts
+++ src/store/modules/global.ts
@@ -10,15 +10,17 @@ function takeConfigField(key: string, defaultValue: boolean = true) {
 
 export const useGlobalStore = defineStore('global', {
   state: () => {
+    const collapse = takeConfigField('collapse', false)
+    const isMobile = window.innerWidth <= DEVICE_WIDTH
     return {
-      collapse: takeConfigField('collapse', false),
+      collapse: isMobile && !collapse || collapse ,
       isFixedSidebar: takeConfigField('isFixedSidebar'),
       isFixedHeader: takeConfigField('isFixedHeader'),
       isOpenFastNav: takeConfigField('isOpenFastNav'),
       isOpenSetting: false,
       routes: [] as TRouteRowArray,
       cacheViews: [] as string[],
-      isMobile: window.innerWidth <= DEVICE_WIDTH,
+      isMobile
     }
   },
   actions: {
